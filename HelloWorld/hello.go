2d2147a08a859863810eed34bb98c2319f1d5fd4
Paperballin <paperballin@outlook.com>
2022-11-02 17:07:29 +0800

    Hello world and test it!

--- HelloWorld/hello.go
+++ HelloWorld/hello.go
@@ -1,5 +1,29 @@
 package HelloWorld
 
-func Hello(name string) string {
-	return "Hello " + name
+const englishHelloPrefix = "Hello, "
+const chineseHelloPrefix = "你好, "
+const defaultEnglishName = "World"
+const defaultChineseName = "世界"
+const chinese = "Chinese"
+
+func Hello(name string, language string) string {
+	if name == "" {
+		switch language {
+		case chinese:
+			name = defaultChineseName
+		default:
+			name = defaultEnglishName
+		}
+	}
+	return changeLanguage(language) + name
+}
+
+func changeLanguage(language string) (prefix string) {
+	switch language {
+	case chinese:
+		prefix = chineseHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
--- HelloWorld/hello_test.go
+++ HelloWorld/hello_test.go
@@ -3,8 +3,14 @@ package HelloWorld
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("World")
-	want := "Hello World"
+	t.Run("Say hello to everyone", func(t *testing.T) {
+		got := Hello("", "Chinese")
+		want := "你好, 世界"
+		assertMessage(t, got, want)
+	})
+}
+
+func assertMessage(t testing.TB, got, want string) {
 	if got != want {
 		t.Errorf("We got a %q, but we want a %q", got, want)
 	}
