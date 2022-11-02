e0849dec2d75bf59bce43ac02bd31f5e8a384077
Paperballin <paperballin@outlook.com>
2022-11-02 17:24:03 +0800

    AddNumbers and test it!

---
+++ Integers/addNumbers_test.go
@@ -0,0 +1,15 @@
+package Integers
+
+import "testing"
+
+func TestAddNumbers(t *testing.T) {
+	sum := Add(2, 2)
+	expect := 4
+	assertMessage(t, expect, sum)
+}
+
+func assertMessage(t testing.TB, expect int, sum int) {
+	if sum != expect {
+		t.Errorf("Expect: %d, Got: %d", expect, sum)
+	}
+}
